66b066f093af52150fc341491ae096ac7a6fc298
luigy-mach <luigy.mach.arc@gmail.com>
2017-10-29 08:31:46 -0500

    update

---
+++ install-docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+wait
+echo "--------------------------------------"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+wait
+echo "--------------------------------------"
+
+sudo apt-get update
+wait
+echo "--------------------------------------"
+
+apt-cache policy docker-ce
+wait
+echo "--------------------------------------"
+
+sudo apt-get install -y docker-ce
+wait
+echo "--------------------------------------"
+
+sudo systemctl status docker
